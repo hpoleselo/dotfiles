7f101508893678a3d69f608c5ce5bc99df1cf6bd
Henrique Poleselo <hpoleselo@gmail.com>
2021-05-05 00:53:16 -0300

    Added one parent script to run all scripts.

---
+++ init-ubuntu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+# Run programs by:
+cd shell-scripts
+./delete-bloatware.sh
+./install-packages.sh
+./batch-ppa-download.sh
+./essentials.sh
+./initialize-pyenv.sh
+
+# Echoing
+# This supposes that the git repo has been cloned on the home folder
+#PATH=$PATH:~/dotfiles >> ~/.bashrc
+#echo $PATH
+#source .bashrc
+./echoing.sh
