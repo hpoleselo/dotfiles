2244419b2a0ded0d4c3535f9a94b360e79214c9d
Henrique Poleselo <hpoleselo@gmail.com>
2021-05-10 09:00:50 -0300

    Added rect2polar executable and gcpenv

--- shell-scripts/echoeing.sh
+++ shell-scripts/echoeing.sh
@@ -1,11 +1,10 @@
 # Using functions in bashrc
 #cd ~ | sleep 2 | echo "runVirtualEnv() { export WORKON_HOME=/home/henrivis/.virtualenvs ; source /usr/local/bin/virtualenvwrapper.sh ; workon cv ; }" >> ~/.bashrc | echo "ros() { source /opt/ros/kinetic/setup.bash ; }" >> ~/.bashrc | echo "run_octoprint() { sudo chmod +666 /dev/ttyACM0 ; octoprint serve ; }" >> ~/.bashrc | echo "PATH=$PATH:~/catkin_ws/src/octoROS/src" >> ~/.bashrc | echo "reiniciar_placa_rede() { sudo service network-manager restart ; }" >> ~/.bashrc | echo "matlab() { cd /usr/local/MATLAB/R2017a/bin ; ./matlab ; }" >> ~/.bashrc | echo "surfbot() { cd ~/Documents/Codigos/SurfBot/ ; python3.5 telegramInterface.py ; }" >> ~/.bashrc | echo "ws() { source ~/catkin_ws/devel/setup.bash ; }" >> ~/.bashrc | echo "exportarIp() { export ROS_MASTER_URI='http://192.168.13.150:11311' ; }" >> ~/.bashrc | echo "ur5() { cd ~/InstalledPrograms/ursim-3.12.1.90940 ; ./start-ursim.sh ;}" >> ~/.bashrc  
 
-# Using aliases (better because one can list the aliases!)
 echo "alias runVirtualEnv ='export WORKON_HOME=/home/henrivis/.virtualenvs ; source /usr/local/bin/virtualenvwrapper.sh ; workon cv ;'" >> ~/.bashrc \
 | echo "alias ros='source /opt/ros/melodic/setup.bash ;'" >> ~/.bashrc \
 | echo "alias run_octoprint='sudo chmod +666 /dev/ttyACM0 ; octoprint serve ; '" >> ~/.bashrc \
-| echo "PATH=$PATH:~/catkin_ws/src/octoROS/src" >> ~/.bashrc \
+| echo "PATH=$PATH:~/catkin_ws/src/octoROS/src:/home/henrivis/Documents/Codigos/rect2polar" >> ~/.bashrc \
 | echo "alias reiniciar_placa_rede='sudo service network-manager restart ;'" >> ~/.bashrc \
 | echo "alias matlab='cd /usr/local/MATLAB/R2017a/bin ; ./matlab ;'" >> ~/.bashrc \
 | echo "alias surfbot='cd ~/Documents/Codigos/SurfBot/ ; python3.6 telegramInterface.py ;'" >> ~/.bashrc \
@@ -23,6 +22,6 @@ echo "alias runVirtualEnv ='export WORKON_HOME=/home/henrivis/.virtualenvs ; sou
 | echo "alias pyenv='source ~/Documents/Codigos/py-env/bin/activate ;'" >> ~/.bashrc \
 | echo "alias deslig='sudo shutdown -P +35'" >> ~/.bashrc \
 | echo "alias rfcaster='python ~/Documents/Codigos/telegramForecaster/src/telegram-interface.py'" >> ~/.bashrc \
-| echo "alias comprimir='echo 7z-Example: 7z a filename.7z -p pwd -mhe folder/files'" >> ~/.bashrc 
-
+| echo "alias comprimir='echo 7z-Example: 7z a filename.7z -p pwd -mhe folder/files'" >> ~/.bashrc \
+| echo "alias gcpenv='source ~/Documents/Codigos/gcp-etl/.env'" >> ~/.bashrc
 
