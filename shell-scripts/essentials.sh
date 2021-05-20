014d26631f69ee91c8ccb41ed3bfd5e8cce4207f
Henrique Poleselo <hpoleselo@gmail.com>
2021-05-20 18:05:59 -0300

    Changed shell script to include all fonts within /fonts dir. and added new fonts.

--- shell-scripts/essentials.sh
+++ shell-scripts/essentials.sh
@@ -1,7 +1,3 @@
-# Spotify
-curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-
 # Jupyter Notebook
 sudo -H pip3 install --upgrade pip
 sudo -H pip3 install virtualenv
@@ -10,10 +6,3 @@ cd ~/py_venv
 virtualenv py_venv_env
 source py_venv_env/bin/activate
 pip install jupyter
-
-# Skype
-sudo apt install apt-transport-https
-curl https://repo.skype.com/data/SKYPE-GPG-KEY | sudo apt-key add -
-echo "deb https://repo.skype.com/deb stable main" | sudo tee /etc/apt/sources.list.d/skypeforlinux.list
-sudo apt update
-sudo apt install skypeforlinux
--- shell-scripts/install-fonts-terminator.sh
+++ shell-scripts/install-fonts-terminator.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 echo "---- Installing Fonts from /fonts directory ----"
-# Installing fonts used in terminator profile
-cp ../fonts/CascadiaCode.ttf ~/.local/share/fonts
-cp ../fonts/CascadiaMono.ttf ~/.local/share/fonts
-cp ../fonts/FiraCode-Light.ttf ~/.local/share/fonts
-cp ../fonts/FiraCode-Regular.ttf ~/.local/share/fonts
-cp ../fonts/FontsFree-Net-SFMono-Regular.ttf ~/.local/share/fonts
 
+# Installing fonts used in terminator profile
+cp ../fonts/* ~/.local/share/fonts
 
 echo "---- Importing Terminator profile ----"
 # Importing terminator profile
