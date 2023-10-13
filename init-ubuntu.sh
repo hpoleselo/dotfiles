9e3545a91ea401628457ca4fc155fa8aee404060
Henrique Poleselo <hpoleselo@gmail.com>
2023-10-13 13:12:29 -0300

    Update init-ubuntu.sh
    Removed PPA repositories because Spotify and Typora's are outdated.

--- init-ubuntu.sh
+++ init-ubuntu.sh
@@ -3,8 +3,8 @@
 cd shell-scripts
 sudo sh delete-bloatware.sh
 sudo sh install-packages.sh
-sudo sh batch-ppa-download.sh
-sudo sh echoing.sh
+#sudo sh batch-ppa-download.sh
+#sudo sh echoing.sh
 sudo sh install-fonts-terminator.sh
 
 # TODO: adjuste pyenv
