665335c12a0c76f8b21cb679d686025d086dd945
Henrique Poleselo <hpoleselo@gmail.com>
2024-08-19 16:45:14 -0300

    style: Adds logging between calls.

--- shell-scripts/batch-ppa-download.sh
+++ shell-scripts/batch-ppa-download.sh
@@ -1,10 +1,11 @@
 echo "---- Installing packages from PPA ----"
 
 # Libreoffice
-echo "## Preping Libreoffice ##"
+echo "## Prepping Libreoffice ##"
 sudo add-apt-repository ppa:libreoffice
 
 # Sublime Merge
+echo "## Prepping Sublime Merge ##"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 sudo apt-get install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
